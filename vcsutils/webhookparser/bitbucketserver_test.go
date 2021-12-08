e2bc42de1069c069328feb9a0492bfca7fe2e8ef
Rémi Bourgarel <bourgarel.remi@gmail.com>
2021-12-08 12:56:37 +0100

    synchronize bitbucket server owner format across API

--- vcsutils/webhookparser/bitbucketserver_test.go
+++ vcsutils/webhookparser/bitbucketserver_test.go
@@ -1,9 +1,11 @@
 package webhookparser
 
 import (
+	"fmt"
 	"net/http/httptest"
 	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/jfrog/froggit-go/vcsutils"
@@ -39,10 +41,14 @@ func TestBitbucketServerParseIncomingPushWebhook(t *testing.T) {
 
 	// Check values
 	assert.Equal(t, expectedRepoName, actual.TargetRepositoryDetails.Name)
-	assert.Equal(t, expectedOwner, actual.TargetRepositoryDetails.Owner)
+	assert.Equal(t, formatOwnerForBitbucketServer(expectedOwner), actual.TargetRepositoryDetails.Owner)
 	assert.Equal(t, expectedBranch, actual.TargetBranch)
 	assert.Equal(t, bitbucketServerPushExpectedTime, actual.Timestamp)
 	assert.Equal(t, vcsutils.Push, actual.Event)
+}
+
+func formatOwnerForBitbucketServer(owner string) string {
+	return fmt.Sprintf("~%s", strings.ToUpper(owner))
 }
 
 func TestBitbucketServerParseIncomingPrCreateWebhook(t *testing.T) {
@@ -62,11 +68,11 @@ func TestBitbucketServerParseIncomingPrCreateWebhook(t *testing.T) {
 	// Check values
 	assert.Equal(t, bitbucketServerExpectedPrId, actual.PullRequestId)
 	assert.Equal(t, expectedRepoName, actual.TargetRepositoryDetails.Name)
-	assert.Equal(t, expectedOwner, actual.TargetRepositoryDetails.Owner)
+	assert.Equal(t, formatOwnerForBitbucketServer(expectedOwner), actual.TargetRepositoryDetails.Owner)
 	assert.Equal(t, expectedBranch, actual.TargetBranch)
 	assert.Equal(t, bitbucketServerPrCreateExpectedTime, actual.Timestamp)
 	assert.Equal(t, expectedRepoName, actual.SourceRepositoryDetails.Name)
-	assert.Equal(t, expectedOwner, actual.SourceRepositoryDetails.Owner)
+	assert.Equal(t, formatOwnerForBitbucketServer(expectedOwner), actual.SourceRepositoryDetails.Owner)
 	assert.Equal(t, expectedSourceBranch, actual.SourceBranch)
 	assert.Equal(t, vcsutils.PrCreated, actual.Event)
 }
@@ -88,11 +94,11 @@ func TestBitbucketServerParseIncomingPrUpdateWebhook(t *testing.T) {
 	// Check values
 	assert.Equal(t, bitbucketServerExpectedPrId, actual.PullRequestId)
 	assert.Equal(t, expectedRepoName, actual.TargetRepositoryDetails.Name)
-	assert.Equal(t, expectedOwner, actual.TargetRepositoryDetails.Owner)
+	assert.Equal(t, formatOwnerForBitbucketServer(expectedOwner), actual.TargetRepositoryDetails.Owner)
 	assert.Equal(t, expectedBranch, actual.TargetBranch)
 	assert.Equal(t, bitbucketServerPrUpdateExpectedTime, actual.Timestamp)
 	assert.Equal(t, expectedRepoName, actual.SourceRepositoryDetails.Name)
-	assert.Equal(t, expectedOwner, actual.SourceRepositoryDetails.Owner)
+	assert.Equal(t, formatOwnerForBitbucketServer(expectedOwner), actual.SourceRepositoryDetails.Owner)
 	assert.Equal(t, expectedSourceBranch, actual.SourceBranch)
 	assert.Equal(t, vcsutils.PrEdited, actual.Event)
 }
